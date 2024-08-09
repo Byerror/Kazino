8dbb23900bd4113c33558081b5df1881c51db6d4
Byerror <153293815+Byerror@users.noreply.github.com>
2024-08-09 21:01:08 +0500

    Add files via upload

---
+++ bet_sender.py
@@ -0,0 +1,41 @@
+from pyrogram import Client
+from pyrogram.types import Message
+import asyncio
+from config import CHANNEL_ID
+
+api_id = ""#сюда свой апи ид
+api_hash = ""#а сюда апи хеш
+
+app = Client('bot', api_id=api_id, api_hash=api_hash)
+
+async def send_bet(username, summa, bet_type) -> int:
+    await app.start()
+
+    message_text = (
+        "**[<emoji id=5282939632416206153>⛈</emoji>] Новая ставка!\n\n"
+        f"> <emoji id=5258011929993026890>👤</emoji> Никнейм игрока: {username}\n\n"
+        f"> <emoji id=5443038326535759644>💬</emoji> Игрок ставит на: {bet_type}\n\n"
+        f"> <emoji id=5375296873982604963>💰</emoji> Сумма ставки: {summa}$**"
+    )
+
+    try:
+        message: Message = await app.send_message(
+            chat_id=CHANNEL_ID,
+            text=message_text
+        )
+
+        return message.id
+    finally:
+        await app.stop()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(send_bet('testusername', 100, 'больше'))
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        try:
+            loop.run_until_complete(app.stop())
+        except Exception as e:
+            print(f"An error occurred while stopping the client: {e}")
---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = "7405917761:AAGAkjiyPaLPRVrVzWPQQAymv11eAZkifi0" # Токен бота
+CHANNEL_ID = -1002129853604 # ID канала со ставками
+ADMINS = [6584286070] # ID админов
+BET_URL = "t.me/send?start=IVrAV6WWx3WV" # Ссылка на счет
+CRYPTOPAY_TOKEN = "244808:AAgSdAyAbLUcSYkF8nJ8tZUP9GeYe3lEKrB" # Тут CryptoPay токен
+WIN_IMAGE = "win.jpg" # Название файла с картинкой выигрыша
+LOSE_IMAGE = "lose.jpg" # Название файла с картинкой проигрыша
+LOGS_ID = -1002195516136 # ID канала с оплатами
+
+RULES_LINK = "https://t.me/igriwa" # Ссылка на канал с правилами
+NEWS_LINK = "https://t.me/newscryptogamecc" # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/SC999AM" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "https://t.me/cryptogamecc" # Ссылка на переходник
+BETS_LINK = "https://t.me/cryptogamecc" # Ссылка на канал ставок
+CASINO_NAME = "Crypto Casino" # Название казино
